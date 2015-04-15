1b10d0fb3f505f1403e47437459a14f3c7401734
Winland <winland0704@126.com>
2015-04-16 00:54:03 +0800

    ch2.5

---
+++ QtProjects/ch02/testbug/widget.cpp
@@ -0,0 +1,23 @@
+﻿#include "widget.h"
+#include "ui_widget.h"
+#include <QtTest/QTest> //added new line
+#include <QDebug>       //added
+
+Widget::Widget(QWidget *parent) :
+    QWidget(parent),
+    ui(new Ui::Widget)
+{
+    //qDebug()<<(void *)(ui->label);      //show the "wild" pointer
+    //ui->label->setText(tr("Test Bug")); //change text
+
+    ui->setupUi(this);
+    qsleep(1000);       //added new line
+
+    //qDebug()<<(void *)(ui->label);    //show normal pointer
+    //qDebug()<<ui->label;              //show label object
+}
+
+Widget::~Widget()
+{
+    delete ui;
+}
